08269f829a8e4dbcc0f22d5fd55da53992b7f934
suvarna-gaware <gawaresuvarna821@gmail.com>
2025-03-28 12:23:28 +0530

    project added to repository
    added by Suvarna

---
+++ Alumni_Event_Management_System/src/main/java/com/example/demo/AlumniEventManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumniEventManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlumniEventManagementSystemApplication.class, args);
+	}
+
+}
