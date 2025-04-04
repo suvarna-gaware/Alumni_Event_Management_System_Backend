58341e1b7d2aa332d7eef0d9378b16c5d6feb94d
suvarna-gaware <gawaresuvarna821@gmail.com>
2025-04-04 11:13:55 +0530

    dbproperty
    by Suvarna

---
+++ Alumni_Event_Management_System/src/main/java/com/example/demo/model/Admin.java
@@ -0,0 +1,12 @@
+package com.example.demo.model;
+
+public class Admin {
+	private int Adminid;
+	private String Username;
+	private String Password;
+	private String Role;
+	private String email;
+	private String contact;
+	
+
+}
---
+++ Alumni_Event_Management_System/src/main/java/com/example/demo/model/Alumni.java
@@ -0,0 +1,13 @@
+package com.example.demo.model;
+
+public class Alumni {
+	private int Alumniid;
+	private int Did;
+	private String name;
+	private String email;
+	private String contact;
+	private String address;
+	private String gender;
+	private int date;
+
+}
